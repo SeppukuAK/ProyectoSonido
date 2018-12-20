56b14e44e1d25fcc88f8eeb87d2feb21e7d39ba5
Raúl Serrano Gómez <raulserranodev@gmail.com>
2018-12-20 10:19:33 +0100

    FIRST COMMIT

---
+++ FMOD/Assets/Project/Scripts/AudioGeometryBox.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class AudioGeometryBox : MonoBehaviour
+{
+    public float DirectOcclusion;// 0.0 no atenua, 1.0 atenua totalmente
+    public float ReverbOcclusion;// atenuacion de la reverberacion
+    public bool DoubleSided; // atenua por ambos lados o no 
+
+    private FMOD.Geometry geometry;
+    private int maxPoligons, maxVertices;
+
+    private BoxCollider boxCollider;
+
+    /// <summary>
+    /// Crea la geometria
+    /// </summary>
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();    
+    }
+
+    private void Start()
+    {
+        maxVertices = 8; // numero de vertices (>=3)
+
+        FMOD.VECTOR[] vertices = new FMOD.VECTOR[maxVertices];
+
+        //TODO: CREAR VERTICES
+        //TODOS LOS VERTICES EN EL MISMO PLANO
+        //POLIGONOS CONVEXOS
+        //POLIGONOS CON AREA POSITIVA
+
+        Vector3[] vertexCollider = GetColliderVertexPositions();
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new FMOD.VECTOR();
+            vertices[i].x = vertexCollider[i].x;
+            vertices[i].y = vertexCollider[i].y;
+            vertices[i].z = vertexCollider[i].z;
+
+            Debug.Log("Vertice " + i + " : x: " + vertices[i].x + " y: " + vertices[i].y + " z: " + vertices[i].z);
+        }
+
+        maxPoligons = 12;
+        geometry = LowLevelSystem.Instance.CreateGeometry(maxPoligons, maxVertices);
+
+        //TODO: PONER POSICIONES
+        int polygonIndex; // Indice al poligono generado para referenciarlo despues
+        LowLevelSystem.ERRCHECK(geometry.addPolygon(DirectOcclusion, ReverbOcclusion, DoubleSided, maxVertices, vertices, out polygonIndex));
+    }
+
+    //TODO: SENTIDO ANTIHORARIO Y SUPERFICIES CONVEXAS
+    private Vector3[] GetColliderVertexPositions()
+    {
+        Vector3[] vertices = new Vector3[8];
+
+        vertices[0] = transform.TransformPoint(boxCollider.center + new Vector3(-boxCollider.size.x, -boxCollider.size.y, -boxCollider.size.z) * 0.5f);
+        vertices[1] = transform.TransformPoint(boxCollider.center + new Vector3(boxCollider.size.x, -boxCollider.size.y, -boxCollider.size.z) * 0.5f);
+        vertices[2] = transform.TransformPoint(boxCollider.center + new Vector3(boxCollider.size.x, -boxCollider.size.y, boxCollider.size.z) * 0.5f);
+        vertices[3] = transform.TransformPoint(boxCollider.center + new Vector3(-boxCollider.size.x, -boxCollider.size.y, boxCollider.size.z) * 0.5f);
+        vertices[4] = transform.TransformPoint(boxCollider.center + new Vector3(-boxCollider.size.x, boxCollider.size.y, -boxCollider.size.z) * 0.5f);
+        vertices[5] = transform.TransformPoint(boxCollider.center + new Vector3(boxCollider.size.x, boxCollider.size.y, -boxCollider.size.z) * 0.5f);
+        vertices[6] = transform.TransformPoint(boxCollider.center + new Vector3(boxCollider.size.x, boxCollider.size.y, boxCollider.size.z) * 0.5f);
+        vertices[7] = transform.TransformPoint(boxCollider.center + new Vector3(-boxCollider.size.x, boxCollider.size.y, boxCollider.size.z) * 0.5f);
+
+        return vertices;
+    }
+}
---
+++ FMOD/Assets/Project/Scripts/LowLevelSystem.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Motor de audio que permite el posicionamiento 3D, geometría y reverbs
+/// Inicializa la librería. Se encarga de cargar y reproducir sonidos.
+/// </summary>
+public class LowLevelSystem : MonoBehaviour
+{
+    public static LowLevelSystem Instance;
+
+    /// <summary>
+    /// Instancia de LowLevelSystem
+    /// </summary>
+    private FMOD.System system;
+    private FMOD.ChannelGroup masterChannel;
+
+    /// <summary>
+    /// Obtiene la instancia del LowLevelSystem
+    /// </summary>
+    private void Awake()
+    {
+        Instance = this;
+
+        //LowLevel
+        system = FMODUnity.RuntimeManager.LowlevelSystem;
+        uint version;
+        ERRCHECK(system.getVersion(out version));
+        Debug.Log("LowLevelSystem version: " + version);
+    }
+
+    void Update()
+    {
+        //TODO: REVISAR SI HACE FALTA
+        ERRCHECK(system.update());
+    }
+
+    /// <summary>
+    /// Crea un sonido 3D
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public FMOD.Sound Create3DSound(string name)
+    {
+        FMOD.Sound sound;
+        //TODO: COMPROBAR
+        ERRCHECK(system.createSound("Assets/" + name + ".wav", FMOD.MODE._3D | FMOD.MODE.LOOP_NORMAL, out sound));
+        return sound;
+    }
+
+    #region Channel
+
+    /// <summary>
+    /// Crea un canal asociado al sonido
+    /// </summary>
+    /// <param name="sound"></param>
+    /// <returns></returns>
+    public FMOD.Channel CreateChannel(FMOD.Sound sound)
+    {
+        FMOD.Channel channel;
+        //Se crea el canal
+        ERRCHECK(system.playSound(
+             sound,
+             masterChannel, // grupo de canales, 0 sin agrupar (agrupado en el master)
+             true, // arranca con "pause" 
+             out channel));
+
+        ERRCHECK(channel.setLoopCount(0));
+        return channel;
+    }
+
+    /// <summary>
+    /// Crea un grupo de canales con un nombre
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public FMOD.ChannelGroup CreateChannelGroup(string name)
+    {
+        FMOD.ChannelGroup channelGroup;
+        ERRCHECK(system.createChannelGroup(name,out channelGroup));
+        return channelGroup;
+    }
+
+    #endregion Channel
+
+    public FMOD.Geometry CreateGeometry(int maxPoligons, int maxVertex)
+    {
+        FMOD.Geometry geometry;
+        ERRCHECK(system.createGeometry(maxPoligons, maxVertex, out geometry));
+        return geometry;
+    }
+
+    /// <summary>
+    /// Facilita la gestión de errores
+    /// </summary>
+    /// <param name="result"></param>
+    public static void ERRCHECK(FMOD.RESULT result)
+    {
+        if (result != FMOD.RESULT.OK)
+            Debug.LogError(result);
+    }
+}
