5999f3f94cea8d32d4c597a2e7e7db2a0f5bec3f
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-01-03 18:10:13 +0100

    Todas las reverbs

--- Project/Assets/Game/Scripts/FMODLowLevel/Reverb.cs
+++ Project/Assets/Game/Scripts/FMODLowLevel/Reverb.cs
@@ -4,9 +4,9 @@
 /// Hace que el sonido persista aunque se haya dejado de emitir el sonido
 /// Son como ecos y dependen del recinto
 /// </summary>
-public class Reverb : MonoBehaviour {
-
-    private enum Presets { ALLEY, ARENA, AUDITORIUM, BATHROOM ,NONE};
+public class Reverb : MonoBehaviour
+{
+    private enum Presets { ALLEY, ARENA, AUDITORIUM, BATHROOM, CARPETTEDHALLWAY, CAVE, CITY, CONCERTHALL, FOREST, GENERIC, HALLWAY, HANGAR, LIVINGROOM, MOUNTAINS, OFF, PADDEDCELL, PARKINGLOT, PLAIN, QUARRY, ROOM, SEWERPIPE, STONECORRIDOR, STONEROOM, UNDERWATER, NONE };
 
     [SerializeField] private SphereCollider minSphere;
     [SerializeField] private SphereCollider maxSphere;
@@ -14,7 +14,7 @@ public class Reverb : MonoBehaviour {
     /// <summary>
     /// Preset inicial
     /// </summary>
-    [SerializeField] private Presets preset;
+    [SerializeField] private Presets preset = Presets.NONE;
 
     private FMOD.Reverb3D reverb;
     private FMOD.REVERB_PROPERTIES reverbProperties;
@@ -25,8 +25,8 @@ public class Reverb : MonoBehaviour {
     private void Start()
     {
         reverb = LowLevelSystem.Instance.CreateReverb();
-      
-        switch(preset)
+
+        switch (preset)
         {
             case Presets.ALLEY:
                 reverbProperties = FMOD.PRESET.ALLEY();
@@ -39,6 +39,66 @@ public class Reverb : MonoBehaviour {
                 break;
             case Presets.BATHROOM:
                 reverbProperties = FMOD.PRESET.BATHROOM();
+                break;
+            case Presets.CARPETTEDHALLWAY:
+                reverbProperties = FMOD.PRESET.CARPETTEDHALLWAY();
+                break;
+            case Presets.CAVE:
+                reverbProperties = FMOD.PRESET.CAVE();
+                break;
+            case Presets.CITY:
+                reverbProperties = FMOD.PRESET.CITY();
+                break;
+            case Presets.CONCERTHALL:
+                reverbProperties = FMOD.PRESET.CONCERTHALL();
+                break;
+            case Presets.FOREST:
+                reverbProperties = FMOD.PRESET.FOREST();
+                break;
+            case Presets.GENERIC:
+                reverbProperties = FMOD.PRESET.GENERIC();
+                break;
+            case Presets.HALLWAY:
+                reverbProperties = FMOD.PRESET.HALLWAY();
+                break;
+            case Presets.HANGAR:
+                reverbProperties = FMOD.PRESET.HANGAR();
+                break;
+            case Presets.LIVINGROOM:
+                reverbProperties = FMOD.PRESET.LIVINGROOM();
+                break;
+            case Presets.MOUNTAINS:
+                reverbProperties = FMOD.PRESET.MOUNTAINS();
+                break;
+            case Presets.OFF:
+                reverbProperties = FMOD.PRESET.OFF();
+                break;
+            case Presets.PADDEDCELL:
+                reverbProperties = FMOD.PRESET.PADDEDCELL();
+                break;
+            case Presets.PARKINGLOT:
+                reverbProperties = FMOD.PRESET.PARKINGLOT();
+                break;
+            case Presets.PLAIN:
+                reverbProperties = FMOD.PRESET.PLAIN();
+                break;
+            case Presets.QUARRY:
+                reverbProperties = FMOD.PRESET.QUARRY();
+                break;
+            case Presets.ROOM:
+                reverbProperties = FMOD.PRESET.ROOM();
+                break;
+            case Presets.SEWERPIPE:
+                reverbProperties = FMOD.PRESET.SEWERPIPE();
+                break;
+            case Presets.STONECORRIDOR:
+                reverbProperties = FMOD.PRESET.STONECORRIDOR();
+                break;
+            case Presets.STONEROOM:
+                reverbProperties = FMOD.PRESET.STONEROOM();
+                break;
+            case Presets.UNDERWATER:
+                reverbProperties = FMOD.PRESET.UNDERWATER();
                 break;
             case Presets.NONE:
                 reverbProperties = new FMOD.REVERB_PROPERTIES();
