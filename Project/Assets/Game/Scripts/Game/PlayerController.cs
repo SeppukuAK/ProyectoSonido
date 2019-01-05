2a0e79fa41bf8e65102864275f62c74e60f9f7dc
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-01-05 13:28:11 +0100

    Añadido standart asset

---
+++ Project/Assets/Game/Scripts/Game/PlayerController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class PlayerController : UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController
+{
+    [Header("Sounds")]
+    [SerializeField] private Sound3D jumpSound;
+
+}
